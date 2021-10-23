1bb71ecb742c77ce553f26e18411298c7ee8c589
Poornima JD <31023599+poornimajd@users.noreply.github.com>
2021-10-23 11:19:38 +0530

    Update robof.py

--- robof.py
+++ robof.py
@@ -5,7 +5,7 @@ if __name__=='__main__':
     rospy.init_node('my_first_node')##initializing the node
     rospy.loginfo("this is my first node")
     # rospy.sleep(1)
-    # rospy.loginfo('heheh')
+    # rospy.loginfo('heheh')#ok done
     rate=rospy.Rate(10)#10 times a sec,its like freq
     # print(rate)
     while not rospy.is_shutdown():###when u ill the node it is shut
